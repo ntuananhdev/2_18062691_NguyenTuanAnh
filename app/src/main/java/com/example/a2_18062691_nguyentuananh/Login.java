0d14803bbb9b03ea5f80655ba2ba115d129cb09a
ntuananhdev <tuananh.forwork@gmail.com>
2021-06-01 11:38:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a2_18062691_nguyentuananh/Login.java
@@ -0,0 +1,14 @@
+package com.example.a2_18062691_nguyentuananh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
